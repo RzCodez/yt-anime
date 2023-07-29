72948b2c846166ca43d8e5c501995911cf5cdb3f
_userNull <ramzprivatexd@gmail.com>
2023-07-29 17:56:07 +0700

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 import { gsap } from 'gsap';
 
 
+window.onload = () => {
+    gsap.from(".animate", {
+        duration: 1,
+        opacity: 0,
+        y: 30,
+        ease: "power3.out"
+    })
+    
+}
