226db8ae19aeb9b2aabb540ef5f9d8cb886fe481
_userNull <ramzprivatexd@gmail.com>
2023-07-30 16:07:52 +0700

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,65 @@
 import { gsap } from 'gsap';
+import { TextPlugin } from "gsap/TextPlugin";
+
+gsap.registerPlugin(TextPlugin);
 
 
 window.onload = () => {
-    gsap.from(".animate", {
+
+    const black = document.querySelector(".black");
+    black.style.display = "none";
+    let tl = gsap.timeline();
+
+    tl.to(".copyright", {
         duration: 1,
-        opacity: 0,
-        y: 30,
-        ease: "power3.out"
+        text: {
+            value: "ANIMECODE© 2023",
+        }
     })
+    tl.from(".img-container", {
+        duration: 1.7,
+        clipPath: "polygon(100% 0, 100% 0, 100% 100%, 100% 100%)",
+        ease: "expo.inOut",
+        
+    })
+
+    .from(".images", {
+        duration: 1.7,
+        transform: "translateX(-20%) scale(1.5)",
+        ease: "expo.inOut",
+    }, 1)
+
+
+    .from(".picture", {
+        duration: 3,
+        "--opacity": 0,
+        "--scale": 0,
+        ease: "power1.inOut",
+    }, 1.4)
+
+    .from(".source", {
+        duration: 1,
+        x: 30,
+        opacity: 0,
+        ease: "expo.out",
+    }, 2)
+
+    .from(".wip", {
+        duration: 1,
+        x: -30,
+        opacity: 0,
+        ease: "expo.out",
+        stagger: 0.2,
+    }, 2)
+
+    .from(".menu", {
+        duration: 1,
+        y: 10,
+        opacity: 0,
+        ease: "expo.out",
+        stagger: 0.2,
+    }, 2)
+
+
     
 }
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite"
+export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+  },
+
+ build: {
+    outDir: "dist",
+    minify: true,
+    rollupOptions: {
+      input: "assets/js/script.js",
+      output: {
+        entryFileNames: "[name]-[hash].js",
+        chunkFileNames: "[name]-[hash].js",
+        assetFileNames: '[name]-[hash][extname]?v=[hash]',
+      },
+    },
+  },
+})
